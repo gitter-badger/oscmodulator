74f3fcffabbbb8e8f536dba5c0d576e2c7bd6c95
Dan Hansen <hansen.danr@gmail.com>
2013-10-06 22:22:25 -0600

    Upgrade package dependencies, update Gruntfile.js, karma conf, and node-webkit

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -1,63 +1,62 @@
 /* jshint quotmark:single */
 // Karma E2E configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-// base path, that will be used to resolve files and exclude
-basePath = '../';
+module.exports = function(config) {
+  'use strict';
 
-// list of files / patterns to load in the browser
-files = [
-//  JASMINE, // TODO I don't believe we need these jasmine files.
-//  JASMINE_ADAPTER,
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  {pattern:'app/scripts/**/*.js', watched:true, included:false, served:false},
-  {pattern:'app/views/**/*.html', watched:true, included:false, served:false},
-  'test/e2e/**/*.js'
-];
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
 
-// list of files to exclude
-exclude = [];
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['ng-scenario'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    reporters: ['spec'],
 
-// web server port
-port = 8080;
+    // list of files / patterns to load in the browser
+    files: [
+      {pattern:'app/scripts/**/*.*', watched:true, included:false, served:false},
+      {pattern:'app/views/**/*.html', watched:true, included:false, served:false},
+      'test/e2e/**/*.js'
+    ],
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to exclude
+    exclude: [],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
-// Needs to match the port on which the grunt connect task launches the server
-proxies = {
-  '/' : 'http://localhost:9001/'
+    // Needs to match the port on which the grunt connect task launches the server
+    proxies: {
+      '/' : 'http://localhost:9000/'
+    },
+
+    // URL root prevent conflicts with the site root
+    urlRoot: '_karma_'
+  });
 };
-
-urlRoot = '/karma/';
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,68 +1,69 @@
 /*jshint quotmark:single*/
 // Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-// base path, that will be used to resolve files and exclude
-basePath = '../app';
+module.exports = function(config) {
+  'use strict';
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
 
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/jquery/jquery.js',
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/angular-bootstrap/ui-bootstrap-tpls.js',
-//  'app/config.js',
-//  'app/config_overrides.js',
-  'scripts/app.js',
-  'scripts/**/*.js',
-  '../test/spec/**/*.js',
-  'views/*.html'
-];
+    reporters: ['spec'],
 
-// The html2js preprossessor is used to load template files
-// during testing so directives can be unit tested.
-preprocessors = {
-  '**/*.html': 'html2js'
+    // list of files / patterns to load in the browser
+    files: [
+      'app/components/jquery/jquery.js',
+      'app/components/angular/angular.js',
+      'app/components/angular-mocks/angular-mocks.js',
+      'app/components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js',
+      'app/views/*.html'
+    ],
+
+    // The html2js preprossessor is used to load template files
+    // during testing so directives can be unit tested.
+    preprocessors: {
+      '**/*.html': 'ng-html2js'
+    },
+
+    ngHtml2JsPreprocessor: {
+      stripPrefix: 'app/'
+    },
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 8080,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
