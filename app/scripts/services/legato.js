8e678a82bca7698debe9e4c108604f74b26b8fc4
Dan Hansen <hansen.danr@gmail.com>
2013-10-15 21:19:31 -0600

    Add npm to fix install command

--- app/scripts/services/legato.js
+++ app/scripts/services/legato.js
@@ -2,6 +2,7 @@
 
 angular.module('oscmodulatorApp').factory('legato', function (node) {
   var legato = node.require('legato');
+  //TODO figure out service mocks for e2e
   if (legato) {
     legato.init();
   }
