6a90bfb6b0194dd3642053206e4e8c8d89e0627e
Dan Hansen <hansen.danr@gmail.com>
2013-11-06 21:46:24 -0700

    Switch to using OSCModulator fork of legato
    * Also enabled support for node modules that use .coffee files

--- app/scripts/services/node.js
+++ app/scripts/services/node.js
@@ -1,15 +1,22 @@
 'use strict';
 
 angular.module('oscmodulatorApp').factory('node', function () {
+
+  var isNodeEnvironment = false;
+  if (typeof require === 'function') {
+    isNodeEnvironment = true;
+    // Enables support for .coffee files in node modules
+    require('coffee-script');
+  }
+
   return {
     require: function(module) {
       // TODO If we can mock out the node dependencies, do we actually need this case statement?
       // TODO If we keep this statement, should we throw an exception if require doesn't exist so it's easier to
       // tell what happened?
-      if (typeof require !== 'function') {
-        return;
+      if (isNodeEnvironment) {
+        return require(module);
       }
-      return require(module);
     }
   };
 });
